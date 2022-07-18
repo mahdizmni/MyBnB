5f9e685697136e11097acc676d0c32bc2b896ca0
mahdizmni <mahdizmni@yahoo.com>
2022-07-18 11:00:16 -0400

    init

---
+++ init.sql
@@ -0,0 +1,101 @@
+-- Tables 
+
+-- TODO: contraints
+
+CREATE TABLE Listings (
+    type VARCHAR() NOT NULL            -- putting constraint for options in sql or code?
+    latitude DECIMAL(10, 8)            -- https://yeahexp.com/data-type-for-latitude-and-longitude/
+    longitude DECIMAL(11, 8) 
+);
+
+CREATE TABLE Rated on User(
+
+
+)
+
+CREATE TABLE (
+
+
+)
+
+CREATE TABLE (
+
+
+)
+
+CREATE TABLE (
+
+
+)
+
+CREATE TABLE (
+
+
+)
+
+CREATE TABLE (
+
+
+)
+
+CREATE TABLE (
+
+
+)
+
+CREATE TABLE (
+
+
+)
+
+CREATE TABLE (
+
+
+)
+
+CREATE TABLE (
+
+
+)
+
+CREATE TABLE (
+
+
+)
+
+
+CREATE TABLE (
+
+
+)
+
+CREATE TABLE (
+
+
+)
+
+CREATE TABLE (
+
+
+)
+
+CREATE TABLE (
+
+
+)
+
+
+CREATE TABLE (
+
+
+)
+
+CREATE TABLE (
+
+
+)
+
+CREATE TABLE (
+
+
+)
