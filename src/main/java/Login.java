207672943ed52d3e3112a99652b56ae97a06d83f
peetacho <peterchow2001@gmail.com>
2022-07-24 18:30:04 -0400

    Create Login.java

---
+++ src/main/java/Login.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Login {
+    public static User login(){
+        Scanner scan = new Scanner(System.in);
+        Utils.printDivider("Login");
+        System.out.print("Email: ");
+        String emailInput = scan.next();
+        System.out.print("Password: ");
+        String passwordInput = scan.next();
+        User user;
+        try{
+            if((user = MySQLObj.validateLoginCredentials(emailInput, passwordInput)) != null){
+                return user;
+            }
+            else{
+                Utils.printInfo("Login credentials are incorrect, please try again!");
+            }
+        }
+        catch (Exception e){
+            Utils.printError("Something went wrong!", e.getCause());
+        }
+        return null;
+    }
+}
