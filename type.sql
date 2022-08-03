258e904dcb59dfefa4bfa4574bef34ed6c078860
peetacho <peterchow2001@gmail.com>
2022-08-03 17:30:51 -0400

    added sample data sql files

---
+++ type.sql
@@ -0,0 +1,8 @@
+INSERT INTO Type VALUES(1, 'House');
+INSERT INTO Type VALUES(2, 'Room');
+INSERT INTO Type VALUES(3, 'Bungalow');
+INSERT INTO Type VALUES(4, 'Townhouse');
+INSERT INTO Type VALUES(5, 'Condo');
+INSERT INTO Type VALUES(6, 'Castle');
+INSERT INTO Type VALUES(7, 'Cabin');
+INSERT INTO Type VALUES(8, 'Cottage');
