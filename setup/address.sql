2bb1637012e9fa0596d537f73ba818d5f2740e5c
peetacho <peterchow2001@gmail.com>
2022-08-04 15:39:12 -0400

    added more setup

---
+++ setup/address.sql
@@ -0,0 +1,73 @@
+INSERT INTO Address VALUES('9 Channel Nine Ct','M1S 4B5'); -- Scarborough
+INSERT INTO Address VALUES('1510 Markham Rd','M1B 2V9');
+INSERT INTO Address VALUES('60 Tuxedo Ct','M1G 3S3');
+INSERT INTO Address VALUES('875 Morningside Ave','M1C 0C7');
+INSERT INTO Address VALUES('2908 Ellesmere Rd','M1E 4B8');
+INSERT INTO Address VALUES('2867 Ellesmere Rd','M1E 4B9');
+INSERT INTO Address VALUES('1150 Markham Rd','M1H 2Y6');
+INSERT INTO Address VALUES('300 Borough Dr','M1P 4P5');
+INSERT INTO Address VALUES('1615 Ellesmere Rd','M1P 2Y3');
+INSERT INTO Address VALUES('Cafeteria, 100 Borough Dr 1st Fl','M1P 4W2');
+INSERT INTO Address VALUES('1401 Ellesmere Rd','M1P 4R4');
+INSERT INTO Address VALUES('3353 Lawrence Ave E Unit 1','M1H 1A8');
+INSERT INTO Address VALUES('3488 Lawrence Ave E','M1H 1A9');
+INSERT INTO Address VALUES('8129 Sheppard Ave E','M1B 6A3'); -- Scarborough
+INSERT INTO Address VALUES('1800 Sheppard Ave E Unit 1049','M2J 5A7'); --  North York
+INSERT INTO Address VALUES('461 Sheppard Ave E','M2N 3B3');
+INSERT INTO Address VALUES('4841 Yonge St','M2N 5X2');
+INSERT INTO Address VALUES('4750 Yonge Street ','M2N 6G5');
+INSERT INTO Address VALUES('4696 Yonge St','M2N 5M2');
+INSERT INTO Address VALUES('4986 Yonge St','M2N 7G9');
+INSERT INTO Address VALUES('5095 Yonge St','M2N 6Z4');
+INSERT INTO Address VALUES('5383 Yonge St','M2N 5R6');
+INSERT INTO Address VALUES('5571 Yonge St','M2G 5S4');
+INSERT INTO Address VALUES('5695 Yonge St','M2N 3T9');
+INSERT INTO Address VALUES('5765 Yonge St Apt 1401','M5H 2M5');
+INSERT INTO Address VALUES('4110 Yonge St','M2P 2B7');
+INSERT INTO Address VALUES('4025 Yonge St Suite 129','M2P 2E3'); --  North York
+INSERT INTO Address VALUES('657 University Ave','M5G 2C4'); -- Toronto
+INSERT INTO Address VALUES('941 Progress Ave','M1C 3T8');
+INSERT INTO Address VALUES('455 Spadina Ave','M5S 1A1');
+INSERT INTO Address VALUES('444 Yonge Street College Park','M5B 2H4');
+INSERT INTO Address VALUES('175 Dundas St E','M5A 1Z4');
+INSERT INTO Address VALUES('2 Queen St E','M5C 3G7');
+INSERT INTO Address VALUES('323 Richmond St E','M5A 4R3');
+INSERT INTO Address VALUES('55 Front St E','M5E 1B5');
+INSERT INTO Address VALUES('33 Yonge St Unit 109','M5E 1G4');
+INSERT INTO Address VALUES('119 Spadina Ave.','M5V 2L1');
+INSERT INTO Address VALUES('952 King St W','M6K 1E4');
+INSERT INTO Address VALUES('1033 Queen St W','M6J 0A6');
+INSERT INTO Address VALUES('1167 Queen St W','M6J 1J4'); -- Toronto
+INSERT INTO Address VALUES('6075 Creditview Rd','L5V 2A8'); -- Mississauga
+INSERT INTO Address VALUES('4530 Erin Mills Pkwy','L5M 4L9');
+INSERT INTO Address VALUES('Mall With Wendys, 2655 Eglinton Ave West','L5M 7E1');
+INSERT INTO Address VALUES('885 Britannia Rd W','L5V 2X8');
+INSERT INTO Address VALUES('4885 Mavis Rd','L5R 3W4');
+INSERT INTO Address VALUES('44 Britannia Rd E','L4Z 3W7');
+INSERT INTO Address VALUES('30 Eglinton Ave W Unit C14','L5R 3E7');
+INSERT INTO Address VALUES('100 City Centre Dr','L5B 2C9');
+INSERT INTO Address VALUES('3 Robert Speck Pkwy','L4Z 2G5');
+INSERT INTO Address VALUES('3445 Hurontario St','L5A 2H4');
+INSERT INTO Address VALUES('2325 Hurontario St','L5A 4C7');
+INSERT INTO Address VALUES('144 Dundas St W','L5B 1H9');
+INSERT INTO Address VALUES('2427 Cawthra Rd','L5A 2X1');
+INSERT INTO Address VALUES('1327 Dundas St E','L4Y 2C7');
+INSERT INTO Address VALUES('4100 Dixie Rd','L4W 1M3'); -- Mississauga
+INSERT INTO Address VALUES('8801 Woodbine Ave','L3R 5W9'); -- Markham
+INSERT INTO Address VALUES('8291 Woodbine Ave','L3R 2P4');
+INSERT INTO Address VALUES('7828 Kennedy Rd','L3R 5P1');
+INSERT INTO Address VALUES('8 The Seneca Way','L3R 5Y1');
+INSERT INTO Address VALUES('8310 McCowan Rd','L3P 3J3');
+INSERT INTO Address VALUES('5000 Hwy 7','L3R 4M9');
+INSERT INTO Address VALUES('9251 Woodbine Ave','L6C 1Y9'); -- Markham
+INSERT INTO Address VALUES('11005 Yonge St','L4C 3E3'); -- Richmond Hill
+INSERT INTO Address VALUES('10579 Yonge St','L4C 3C5');
+INSERT INTO Address VALUES('10 Trench St','L4C 4Z3');
+INSERT INTO Address VALUES('9700 Yonge St','L4C 1V8');
+INSERT INTO Address VALUES('9350 Yonge St','L4C 5T2');
+INSERT INTO Address VALUES('9184 Yonge St','L4C 7A1');
+INSERT INTO Address VALUES('9275 Bayview Ave Building B','L4C 9X4');
+INSERT INTO Address VALUES('50a Red Maple Rd','L4B 1K1');
+INSERT INTO Address VALUES('9005 Leslie St Unit 1 & 2','L4B 1G7');
+INSERT INTO Address VALUES('10 Vogell Rd Unit 1','L4B 3K4');
+INSERT INTO Address VALUES('1410 Major Mackenzie Dr E','L4S 0A1'); -- Richmond Hill
